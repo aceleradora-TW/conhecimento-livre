7b3974cb3884db15f1797dfc1ca4ab93779423d3
hsangalli <habyte@gmail.com>
2017-09-26 14:18:24 -0300

    adiciona cool

--- hello_world.js
+++ hello_world.js
@@ -1,11 +1,23 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const app = express();
 const sendString = require('./sendString')
 
+app.set('port', (process.env.PORT || 3000))
 app.get('/', function(req,res ){
   res.send(sendString.imprimeString())
 })
 
-app.listen(3000, function(){
-  console.log('alegria')
+app.get('/cool', function(request, response) {
+  response.send(cool());
+})
+
+
+//
+// app.listen(3000, function(){
+//   console.log('alegria')
+// })
+
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port ' + app.get('port'))
 })
