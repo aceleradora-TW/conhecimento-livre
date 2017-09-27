f4485382d8d72d8db6048f21c1f676967253234b
desireemenezes <deemenzes@hotmail.com>
2017-09-27 15:57:28 -0300

    adiciona pagina incial

--- pag_inicial.js
+++ pag_inicial.js
@@ -1,13 +1,13 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
-const sendString = require('./send_string')
 
 const app = express()
+const path = require('path');
 
 app.set('port', (process.env.PORT || 3000))
 
 app.get('/', (req, res) => {
-  res.send(sendString.imprimeString())
+  res.sendFile(path.join(`${__dirname}/index.html`));
 })
 
 app.get('/cool', (request, response) => {
