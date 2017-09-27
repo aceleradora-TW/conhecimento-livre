fd0fb8b33d32ac6d56d381dcc1f1064bd2e181bf
AndreiRupertti <arupertti@gmail.com>
2017-09-27 16:51:14 -0300

    adiciona paginas dos videos

--- pag_inicial.js
+++ pag_inicial.js
@@ -1,13 +1,26 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
+const path = require('path');
 
 const app = express()
-const path = require('path');
 
 app.set('port', (process.env.PORT || 3000))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/index.html`));
+})
+
+app.get('/video1', (req, res) => {
+  res.sendfile(path.join(`${__dirname}/video1.html`))
+})
+app.get('/video2', (req, res) => {
+  res.sendfile(path.join(`${__dirname}/video2.html`))
+})
+app.get('/video3', (req, res) => {
+  res.sendfile(path.join(`${__dirname}/video3.html`))
+})
+app.get('/video4', (req, res) => {
+  res.sendfile(path.join(`${__dirname}/video4.html`))
 })
 
 app.get('/cool', (request, response) => {
