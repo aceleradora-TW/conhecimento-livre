b84a9224d38e27b4f15297b83f9d10c696a19e31
fealmeida <fe.shakti@hotmail.com>
2017-10-02 14:47:21 -0300

    <Fernanda,Jessica,Maurcio> insere dados do curso

--- test/course_mapper_test.js
+++ test/course_mapper_test.js
@@ -1,22 +1,40 @@
 const expect = require('chai').expect
 const CourseMapper = require('../src/mappers/course_mapper')
 
-describe('CourseMapper', function() {
-  it('return courses', function() {
-    let db = {
-      collection: function(collectionName) {
-        return {
-          find: function() {
-            return [{}, {}, {}]
-          }
+describe.only('CourseMapper', function() {
+
+  let db = {
+    collection: function(collectionName) {
+      return {
+        find: function() {
+          return [{}, {}, {}]
+        },
+        insert: function(nome) {
+          return nome
         }
       }
     }
+  }
+
+  it('return courses', function() {
 
     let courseMapper = new CourseMapper(db)
 
     const courses = courseMapper.findAll()
-
     expect(courses.length).to.equal(3)
+  })
+
+  it('return data couse', function() {
+
+    let courseMapper = new CourseMapper(db)
+
+    const data = courseMapper.insertData('Node',3.5,'Node course','Waldeco')
+
+    expect(data.courseName).to.equal('Node')
+    expect(data.courseDuration).to.equal('3.5')
+    expect(data.courseDescription).to.equal('Node course')
+    expect(data.courseAuthor).to.equal('Waldeco')
+
+
   })
 })
