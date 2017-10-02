41d07c3a78168f1daf647d4ef6104d054c13f344
fealmeida <fe.shakti@hotmail.com>
2017-10-02 14:47:21 -0300

    <Fernanda,Jessica,Maurcio> insere dados do curso

--- src/mappers/course_mapper.js
+++ src/mappers/course_mapper.js
@@ -2,7 +2,11 @@ class CourseMapper {
   constructor(db) { this.db = db }
 
   findAll() {
-    return this.db.collection('courses').find()
+    return this.db.collection('Courses').find()
+  }
+
+  insertData(courseName, courseDuration, courseDescription, courseAuthor) {
+    return this.db.collection('Courses').insert({ courseName: `${courseName}`, courseDuration: `${courseDuration}`, courseDescription: `${courseDescription}`, courseAuthor: `${courseAuthor}` })
   }
 }
 
