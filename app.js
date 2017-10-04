e5e0b967d0450eb9d67d62df9f3381f951179d8c
ClaudiaStrm <s_moura@hotmail.com>
2017-10-04 15:52:52 -0300

    <Claudia, Mauricio>refatora branch arquiteturaMVC

--- app.js
+++ app.js
@@ -5,29 +5,15 @@ const mongoose = require('mongoose')
 const Course = require('./models/course')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
+const videos = require('./models/videosDb')
 
 const app = express()
-<<<<<<< ea54d51822590f909cab36a8f0b6ffc6220087c3
 
 app.use(bodyParser.json())
 
 const MONGO_URL = 'mongodb://localhost:27017/conhecimento-livre-dev'
 app.set('MONGO_URL', (process.env.MONGO_URL || MONGO_URL))
 mongoose.connect(app.get('MONGO_URL'))
-=======
-// const hbs = exphbs.create({
-//   defaultLayout: 'main',
-// })
->>>>>>> comenta código duplicado no arquivo app.js
-
-const videosDb = [{}, { id: 1, nome: 'Node API video 1', url: 'https://www.youtube.com/embed/l4glc0XNVbM' },
-  { id: 2, nome: 'Node API Part II: Retorno do Jedi', url: 'https://www.youtube.com/embed/CTz5pCnZ03k' }]
-
-<<<<<<< ea54d51822590f909cab36a8f0b6ffc6220087c3
-=======
-// app.engine('handlebars', hbs.engine)
-// app.set('view engine', 'handlebars')
->>>>>>> comenta código duplicado no arquivo app.js
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -37,15 +23,16 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(path.join(`${__dirname}/public`)))
 
 app.get('/', (req, res) => {
-  const videos = {
-    videos: videosDb,
-  }
-  res.render('index', videos)
+  res.render('index', { videos })
 })
 
 app.get('/video/:id', (req, res) => {
   const videoId = parseInt(req.params.id, 10)
-  res.render('video', videosDb[videoId])
+  const videoAtual = videos.find(video => video.id === videoId)
+  if (videoAtual === undefined) {
+    res.send('Video não encontrado')
+  }
+  res.render('video', videoAtual)
 })
 
 app.get('/cool', (request, response) => {
---
+++ models/videosDb.js
@@ -0,0 +1,13 @@
+const videosDb = [
+  {
+    id: 1,
+    nome: 'Node API video 1',
+    url: 'https://www.youtube.com/embed/l4glc0XNVbM',
+  },
+  {
+    id: 2,
+    nome: 'Node API Part II: Retorno do Jedi',
+    url: 'https://www.youtube.com/embed/CTz5pCnZ03k',
+  }]
+
+module.exports = videosDb;
