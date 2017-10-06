4eed13d5fba0594e3508f8931046059427a2bef1
fealmeida <fe.shakti@hotmail.com>
2017-10-06 13:41:40 -0300

    <Fernanda, Andrei, Mauricio> adiciona user crud

---
+++ src/mappers/db_mapper.js
@@ -0,0 +1,22 @@
+class DbMapper {
+  constructor(db) { this.db = db }
+
+  findAll() {
+    return this.db.collection('Courses').find() // [Course, Course, Course, ...]
+  }
+
+  insert(courses) {
+    return this.db.collection('Courses').insert(courses.toJSON())
+  }
+  update(content, course) {
+    return this.db.collection('Courses').update(content.toJSON(), course)
+  }
+  getUsers() {
+    return this.db.collection('Users').getUsers() // [User, User, User, ...]
+  }
+  createUser(user, users) {
+    return this.db.collection('Users').createUser(user, users)
+  }
+}
+
+module.exports = DbMapper
