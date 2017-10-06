a65ea43a6d91719880714b8c259530904908ee01
--global <jessicafranke92@gmail.com>
2017-10-06 14:56:48 -0300

    implementa interface

--- test/quick_filter_test.js
+++ test/quick_filter_test.js
@@ -3,21 +3,21 @@ const quickFilter = require('../quick_filter')
 
 describe('quickFilter', function () {
   it('#returnSearch() deve retornar os objetos correspondentes ao level do curso', function() {
-    const objeto1 = { curso: 'js', level: 'beginner'}
-    const objeto2 = { curso: 'js', level: 'advanced'}
-    const objeto3 = { curso: 'js', level: 'beginner'}
+    const objeto1 = { curso: 'js', nivel: 'beginner'}
+    const objeto2 = { curso: 'js', nivel: 'advanced'}
+    const objeto3 = { curso: 'js', nivel: 'beginner'}
     const array = [objeto1, objeto2, objeto3]
     const resultTest = [objeto1, objeto3]
-    const level = 'beginner'
-    expect(quickFilter.returnSearch(level, array).toString()).to.equal(resultTest.toString())
+    const nivel = 'beginner'
+    expect(quickFilter.returnSearch(nivel, array).toString()).to.equal(resultTest.toString())
   })
   it('#returnSearch() deve retornar vazio quando nao ha o parametro beginner', function() {
-    const objeto1 = { curso: 'js', level: 'advanced'}
-    const objeto2 = { curso: 'js', level: 'advanced'}
-    const objeto3 = { curso: 'js', level: 'advanced'}
+    const objeto1 = { curso: 'js', nivel: 'advanced'}
+    const objeto2 = { curso: 'js', nivel: 'advanced'}
+    const objeto3 = { curso: 'js', nivel: 'advanced'}
     const array = [objeto1, objeto2, objeto3]
     const resultTest = 0
-    const level = 'beginner'
-    expect(quickFilter.returnSearch(level, array).length).to.equal(resultTest)
+    const nivel = 'beginner'
+    expect(quickFilter.returnSearch(nivel, array).length).to.equal(resultTest)
   })
 })
