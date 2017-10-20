ee7a8c692a38283c3f5427cb880f435c2a2e6a3f
ClaudiaStrm <s_moura@hotmail.com>
2017-10-20 13:25:05 -0200

    retira testa-autor

--- app.js
+++ app.js
@@ -65,25 +65,6 @@ app.post('/course', (req, res) => {
   })
 })
 
-app.get('/testa-autor', (req, res) => {
-  const novoAutor = new Author({
-    nome: 'Autor',
-    bio: 'djsoisjaasklsdja',
-    email: 'jdoa@jfidds.com',
-    course: [{
-      title: 'JS',
-      content: [],
-    }],
-  })
-  novoAutor.save((err) => {
-    if (err) {
-      console.log(err)
-    } else {
-      res.send('Novo autor!!!')
-    }
-  })
-})
-
 app.get('/autor', (req, res) => {
   Author.find({}, (err, autores) => {
     if (err) {
