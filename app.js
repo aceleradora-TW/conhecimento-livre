448f9573c831e88f402f8a5b0ce7f022896735f7
fealmeida <fe.shakti@hotmail.com>
2017-10-26 09:44:14 -0200

    <@DesireeMenezes, @MaruricioSalin> cria link na pagina de videos

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const Course = require('./models/course')
+const Author = require('./models/author')
 const bodyParser = require('body-parser')
 
 const exphbs = require('express-handlebars')
+const sassMiddleware = require('node-sass-middleware')
 const routes = require('./src/routes/routes')
 
 const app = express()
 
 const MONGO_URL = 'mongodb://localhost:27017/conhecimento-livre-dev'
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public/css'),
+  force: true,
+  outputStyle: 'compressed',
+  prefix: '/css',
+}))
+
+app.use(express.static('public'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
