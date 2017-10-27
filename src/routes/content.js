1fde9fa6b69d2cd336951dfb8029d8d9092399e1
AndreiRupertti <arupertti@gmail.com>
2017-10-27 16:27:12 -0200

    <@AndreiRupertti, @KarenF> refatora content.js

--- src/routes/content.js
+++ src/routes/content.js
@@ -1,35 +1,28 @@
-const Author = require('../../models/author')
+const Controller = require('../mappers/modelsController')
 
-const content = (Content, Search) => (req, res) => {
+const content = (Content, Author) => (req, res) => {
   const id = req.params.id
-
-  Content.update({ '_id': id }, { '$inc': { 'views': 1 } }, function (err, allContents) {
-    if (err) {
-      console.log(err);
-    }
-  })
-
-  const contentFilter = id => item => item._id.toString() === id.toString()
-  Search.setFilter(contentFilter)
-  Content.find({}, (err, allContents) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const content = Search.filter(allContents, id)
-      Author.find({ author: content[0].author }, (err, author) => {
-        if (err) {
-          console.log(err);
+  const contentModel = new Controller(Content)
+  const authorModel = new Controller(Author)
+  contentModel.findAll((allContents) => {
+    contentModel.findById(id, (contentItem) => {
+      authorModel.findByName(contentItem[0].author, (authorItem) => {
+        if ((allContents || contentItem || authorItem) === null) {
+          res.send('404')
         } else {
-
           author = author[0]
           indexNext = (allContents.indexOf(content[0]) + 1)
           next = allContents[indexNext]
           indexPrevious = (allContents.indexOf(content[0]) - 1)
           previous = allContents[indexPrevious]
-
-          res.render('content', { allContents, content, author, next, previous})
+          res.render('content', { allContents, contentItem, authorItem, next, previous })
         }
       })
+    })
+  })
+  Content.update({ '_id': id }, { '$inc': { 'views': 1 } }, function (err, allContents) {
+    if (err) {
+      console.log(err);
     }
   })
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,14 @@
 const Course = require('../../models/course')
 const Content = require('../../models/content')
 const Author = require('../../models/author')
-const Search = require('../search/search')
 const index = require('./index')
 const content = require('./content')
 const author = require('./author')
 const course = require('./course')
 
-const search = new Search()
-
 const allRoutes = {
   index: index(Course),
-  content: content(Content, search, Author),
+  content: content(Content, Author),
   author: author(Author, Course),
   course: course(Course),
 }
