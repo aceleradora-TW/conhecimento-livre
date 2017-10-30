9bfbfa03446b1c85d64ad661e17e02867437d461
othman853 <anuar.yasser@gmail.com>
2017-10-30 17:27:45 -0200

    <@othman853> exemplo de allContents com Array.prototype.find

--- src/routes/content.js
+++ src/routes/content.js
@@ -1,22 +1,20 @@
-const Controller = require('../mappers/modelsController')
+const Controller = require('../mappers/models_controller')
 
 const content = (Content, Author) => (req, res) => {
   const id = req.params.id
   const contentModel = new Controller(Content)
   const authorModel = new Controller(Author)
-  contentModel.findAll((allContents) => {
-    contentModel.findById(id, (contentItem) => {
-      authorModel.findByName(contentItem.author, (authorItem) => {
-        contentModel.updateViews(id, (views) => {
-          if ((allContents || contentItem || authorItem) === null) {
-            res.send('404')
-          } else {
-            res.render('content', { allContents, contentItem, authorItem })
-          }
-        })
+
+  contentModel.updateViews(id, () => {
+    contentModel.findAll((allContents) => {
+      // Usando Array.prototype.find ao inves de buscar no banco
+      const contentItem = allContents.find(content => content.id === id)
+      authorModel.findByName(contentFromId.author, (authorItem) => {
+        return res.render('content', {allContents, contentItem, authorItem})
       })
     })
   })
+
 }
 
 module.exports = content
