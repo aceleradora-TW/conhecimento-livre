de3bc1f3197dd5708c05a43a086a1295439516b3
karenf <karenf@outlook.com>
2017-10-30 10:48:32 -0200

    <@DesireeMenezes, @KarenF> refatora rota update

--- src/mappers/modelsController.js
+++ src/mappers/modelsController.js
@@ -2,7 +2,6 @@ class ModelsController {
   constructor(model) {
     this.model = model
   }
-
   findAll(fn) {
     this.model.find({}, function (err, all) {
       if (err) {
@@ -13,7 +12,6 @@ class ModelsController {
       }
     })
   }
-
   findById(id, fn) {
     this.model.find({ _id: id }, function (err, item) {
       if (err) {
@@ -24,11 +22,20 @@ class ModelsController {
       }
     })
   }
-
   findByName(name, fn) {
     this.model.find({ author: name }, function (err, item) {
       if (err) {
         console.log(err)
+        fn(null)
+      } else {
+        fn(item)
+      }
+    })
+  }
+  updateViews(id, fn) {
+    this.model.update({ _id: id }, { $inc: { views: 1 } }, function (err, item) {
+      if (err) {
+        console.log(err);
         fn(null)
       } else {
         fn(item)
--- src/routes/content.js
+++ src/routes/content.js
@@ -6,24 +6,21 @@ const content = (Content, Author) => (req, res) => {
   const authorModel = new Controller(Author)
   contentModel.findAll((allContents) => {
     contentModel.findById(id, (contentItem) => {
-      authorModel.findByName(contentItem[0].author, (authorItem) => {
-        if ((allContents || contentItem || authorItem) === null) {
-          res.send('404')
-        } else {
-          author = author[0]
-          indexNext = (allContents.indexOf(content[0]) + 1)
-          next = allContents[indexNext]
-          indexPrevious = (allContents.indexOf(content[0]) - 1)
-          previous = allContents[indexPrevious]
-          res.render('content', { allContents, contentItem, authorItem, next, previous })
-        }
+      authorModel.findByName(contentItem.author, (authorItem) => {
+        contentModel.updateViews(id, (views) => {
+          if ((allContents || contentItem || authorItem) === null) {
+            res.send('404')
+          } else {
+            author = author[0]
+            indexNext = (allContents.indexOf(content[0]) + 1)
+            next = allContents[indexNext]
+            indexPrevious = (allContents.indexOf(content[0]) - 1)
+            previous = allContents[indexPrevious]
+            res.render('content', { allContents, contentItem, authorItem, next, previous })
+          }
+        })
       })
     })
-  })
-  Content.update({ '_id': id }, { '$inc': { 'views': 1 } }, function (err, allContents) {
-    if (err) {
-      console.log(err);
-    }
   })
 }
 
