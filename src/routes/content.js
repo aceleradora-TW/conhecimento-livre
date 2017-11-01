83f100692815caabf525b12ec3209e32d6f759af
karenf <karenf@outlook.com>
2017-11-01 15:29:49 -0200

    <@KarenF, @JessicaFranke> refatora playlist-container da pag content

--- src/routes/content.js
+++ src/routes/content.js
@@ -2,17 +2,25 @@ const Controller = require('../mappers/models_controller')
 
 const content = (Content, Author) => (req, res) => {
   const id = req.params.id
+  let arrayContents = []
   const contentModel = new Controller(Content)
   const authorModel = new Controller(Author)
   contentModel.findAll((allContents) => {
     contentModel.findById(id, (contentItem) => {
-      authorModel.findByName(contentItem.author, (authorItem) => {
-        contentModel.updateViews(id, (views) => {
-          if ((allContents || contentItem || authorItem) === null) {
-            res.send('404')
-          } else {
-            res.render('content', { allContents, contentItem, authorItem })
-          }
+      contentModel.findByTitle(contentItem.title, (titleContent) => {
+        authorModel.findByName(contentItem.author, (authorItem) => {
+          contentModel.updateViews(id, (views) => {
+            if ((allContents || contentItem || authorItem) === null) {
+              res.send('404')
+            } else {
+              for (let i = 0; i < allContents.length; i++) {
+                if (allContents[i].title === titleContent.title) {
+                  arrayContents.push(allContents[i])
+                }
+              }
+              res.render('content', { arrayContents, contentItem, authorItem })
+            }
+          })
         })
       })
     })
