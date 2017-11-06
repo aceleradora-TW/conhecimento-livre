52d55a87d186d7fe84a491b163504d9fbba22ffb
KarenF <karenf@outlook.com>
2017-11-06 15:42:16 -0200

    <@KarenF, @DesireeMenezes> insere header #68

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 const Controller = require('../mappers/models_controller')
 
 const index = Course => (req, res) => {
-  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
   const course = new Controller(Course)
   course.findAll((allContents) => {
-    res.render('index', { allContents, fullUrl })
+    res.render('index', { allContents })
   })
 }
 
