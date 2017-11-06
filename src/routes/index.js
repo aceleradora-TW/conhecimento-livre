89263e665ce937bac95acf0650b41d71e6529930
KarenF <karenf@outlook.com>
2017-11-06 13:24:23 -0200

    <@KarenF, @DesireeMenezes> insere url generica #68

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const Controller = require('../mappers/models_controller')
 
 const index = Course => (req, res) => {
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
   const course = new Controller(Course)
   course.findAll((allContents) => {
-    res.render('index', { allContents })
+    res.render('index', { allContents, fullUrl })
   })
 }
 
