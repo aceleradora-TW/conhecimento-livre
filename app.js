5bee98ee3e476de6ba89deb5af010ab3fe8b4488
KarenF <karenf@outlook.com>
2017-11-21 11:40:07 -0200

    <@KarenF, @luizfetrindade> cria fluxo login #101

--- app.js
+++ app.js
@@ -6,11 +6,8 @@ const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const routes = require('./src/routes/routes')
 
-const session = require('express-session')
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-const Admin = require('./models/admin')
+const passport = require('passport')
+const localStrategy = require('./src/auth/local_strategy')
 
 const app = express()
 
@@ -23,9 +20,9 @@ mongoose.connect(app.get('MONGO_URL'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }))
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(localStrategy)
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -38,53 +35,13 @@ app.get('/', routes.index)
 
 app.get('/admin', routes.admin)
 
-passport.use(new LocalStrategy(
-  function (_, password, cb) {
-    Admin.findOne({ password }, function (err, admin) {
-      if (err) {
-        console.log('deu shit')
-        return cb(null)
-      }
-
-      if (!admin) {
-        console.log('sem iadmin')
-        return cb(null, false)
-      }
-
-      if (admin.password !== password) {
-        console.log('senha invalida');
-        return cb(null, false)
-      }
-
-      console.log('ACERTO MIZERAVI!');
-      return cb(null, admin)
-    })
-  }
-))
-
-app.post('/admin/authorList',
+app.post('/admin/list',
   passport.authenticate('local', { failureRedirect: '/admin' }),
-  (req, res) => {
-    console.log('vamos ver')
-    res.redirect('/loginSuccess')
-  }
-);
+  routes.list
+)
 
-app.get('/loginFailure', function (req, res, next) {
-  res.send('Failed to authenticate');
-});
-
-app.get('/loginSuccess', function (req, res, next) {
-  res.send('Successfully authenticated');
-});
-
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
+passport.serializeUser((user, done) => done(null, user))
+passport.deserializeUser((user, done) => done(null, user))
 
 app.get('/content/:id', routes.content)
 
@@ -92,10 +49,8 @@ app.get('/course/:id', routes.course)
 
 app.get('/author/:id', routes.author)
 
-app.use((req, res, next) => {
-  res.status(404).render('404')
-})
+app.use((req, res) =>
+  res.status(404).render('404'))
 
-app.listen(app.get('port'), () => {
-  console.log(`Node app is running on port ${app.get('port')}`)
-})
+app.listen(app.get('port'), () =>
+  console.log(`Node app is running on port ${app.get('port')}`))
--- models/admin.js
+++ models/admin.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
 
 const Schema = mongoose.Schema
 
 const adminSchema = new Schema({
+  username: String,
   password: String,
 })
 
