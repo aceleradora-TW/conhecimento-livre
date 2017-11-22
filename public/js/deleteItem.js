ba3b11dfc80a3cde1b42df0595c19697533e08ca
ClaudiaStrm <s_moura@hotmail.com>
2017-11-22 12:51:18 -0200

    <ClaudiaStrm, @KarenF> refaz mensagem de alerta do autor

--- public/js/deleteItem.js
+++ public/js/deleteItem.js
@@ -2,7 +2,7 @@ function deleteItem(itemId, collection) {
   var message;
 
   if (collection === 'author') {
-    message = 'Esta operação deletará todos os cursos deste autor. Você tem certeza disto?'
+    message = 'Esta operação deletará este autor. Você tem certeza disto?'
   } else if (collection === 'courses') {
     message = 'Esta operação deletará todos as aulas deste curso. Você tem certeza disto?'
   } else if (collection === 'contents') {
