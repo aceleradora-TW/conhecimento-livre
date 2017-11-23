02e597e427146082d3a83aaed0092092b9e6a7a7
ClaudiaStrm <s_moura@hotmail.com>
2017-11-23 14:44:07 -0200

    <@ClaudiaStrm, @JessicaFranke> Cria formulario para incluir aulas

---
+++ public/js/getURL.js
@@ -0,0 +1,4 @@
+function getURL() {
+ var elem = document.getElementById('url');
+ console.log(elem);
+}
--- src/routes/newContent.js
+++ src/routes/newContent.js
@@ -1,5 +1,17 @@
-const newContent = () => (req, res) => {
-  res.render('contentForm')
+const Controller = require('../mappers/models_controller')
+
+const newContent = Author => (req, res, next) => {
+  // res.render('contentForm', List)
+  const authorController = new Controller(Author)
+  authorController
+    .findAll()
+    .then((allAuthors) => {
+      res.render('contentForm', { allAuthors })
+    })
+    .catch((error) => {
+      console.log(error)
+      next()
+    })
 }
 
 module.exports = newContent
