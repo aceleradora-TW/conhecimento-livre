09d803fcd9afee63434fd8d0095c07ee76bf3819
RenanSchmitt <renanschmitt.ti@gmail.com>
2017-12-01 14:53:57 -0200

    <RenanSchmitt> Refatora Branch

--- test/e2e/criticoVideo.js
+++ test/e2e/criticoVideo.js
@@ -2,19 +2,31 @@ module.exports = {
   'Step one: Acessar o site e clicar em algum curso' : function (client) {
     client
     .url('http://localhost:3000/')
-    .click('div[class=card]')
+    .click('.card')
     .pause(1000)
   },
 
   'Step two: Clicar no botão de começar' : function (client){
     client
-    .click('a[id=buttoncomecar]')
+    .click('#btnInit')
     .pause(1000)
   },
 
   'Step three: Clicar no vídeo para que a aula começe' : function(client){
     client
-    .click('iframe[id=playervideo]')
+    .click('#playerVideo')
+    .pause(2000)
+  },
+
+  'Step Four: Clicar no botão de proximo para acessar a próxima aula' : function(client){
+    client
+    .click('#btnNextClass')
+    .pause(2000)
+  },
+
+  'Step Four: Clicar no botão anterior para acessar a aula anterior' : function(client){
+    client
+    .click('#btnAntClass')
     .pause(2000)
     .end();
   }
