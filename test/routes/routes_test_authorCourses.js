830ccd078ece4b194e931c08561273c16f5c673c
luizfetrindade <luiz.fetrindade@gmail.com>
2017-12-04 16:08:35 -0200

    <@KarenF, @AndreiRupertti> refatora test authorCourse #139

--- test/routes/routes_test_authorCourses.js
+++ test/routes/routes_test_authorCourses.js
@@ -2,30 +2,32 @@ const authorCourses = require('../../src/routes/authorCourses')
 const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
-const db = require('../aux/db_test')
 
 chai.use(sinonChai)
 const expect = chai.expect
 
-const req = { params: { id: 1 }}
-const res = { render: sinon.spy() }
-const next = () => {}
+describe('AuthorCourses', function() {
+  const authorItem = {
+    courses: [{title: 'Goodfellas', description: 'funny how?'}]
+  }
 
-const courses = db.courses
+  const courses = authorItem.courses
 
-const authorController = {
-  findAuthorById: sinon.stub().resolves(db)
-}
+  const req = { params: { id: 1 }}
+  const res = { render: sinon.spy() }
+  const next = function() {}
+  const authorController = {
+    findAuthorById: sinon.stub().resolves(authorItem)
+  }
 
-describe('AuthorCourses', () => {
-  context('com promise resolvida', () => {
-    it('chama findAuthorById com id do request', () => {
+  context('com promise resolvida', function() {
+    it('chama findAuthorById com id do request', function() {
       authorCourses(authorController)(req, res, next)
       expect(authorController.findAuthorById).to.have.been.calledWith(req.params.id)
     })
-    it('renderiza template dos cursos do #authorCourses', () => {
+    it('renderiza template dos cursos do #authorCourses', function() {
       authorCourses(authorController)(req, res, next)
-      expect(res.render).to.have.been.calledWith('authorCourses', { authorItem: db, courses })
+      expect(res.render).to.have.been.calledWith('authorCourses', { authorItem, courses })
     })
   })
 })
