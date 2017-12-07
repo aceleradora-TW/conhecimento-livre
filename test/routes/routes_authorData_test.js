5e9b561bdbc44963fcb118ddc8a367ee633e8cd8
fealmeida <fe.shakti@hotmail.com>
2017-12-07 15:31:56 -0200

    <@fealmeida> arruma padrao

--- test/routes/routes_authorData_test.js
+++ test/routes/routes_authorData_test.js
@@ -7,26 +7,23 @@ chai.use(sinonChai)
 const expect = chai.expect
 
 describe('AuthorData', function() {
-  const authorItem = {
-    name: 'Albis Forenzzi'
-  }
-
+  const authorItem = { name: 'Albis Forenzzi' }
   const req = { params: { id: 1 }}
   const res = { render: sinon.spy() }
   const next = sinon.spy()
 
-  const authorController = {
-    findAuthorById: sinon.stub().resolves(authorItem)
-  }
+  const authorController = { findAuthorById: sinon.stub().resolves(authorItem) }
 
   context('com promise resolvida', function() {
     it('chama findAuthorById com id do request', function() {
       authorData(authorController)(req, res, next)
+
       expect(authorController.findAuthorById).to.have.been.calledWith(req.params.id)
     })
 
     it('renderiza template dos cursos do #authorData', function() {
       authorData(authorController)(req, res, next)
+
       expect(res.render).to.have.been.calledWith('authorForm', { authorItem })
     })
   })
